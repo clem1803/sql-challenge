6e74d63fde92347e04235b7502dfcca4bd434ba2
clem1803 <clementboit@gmail.com>
2021-04-06 18:17:46 -0400

    Tuesday 5pm

--- EmployeeSQL/sql_code.sql
+++ EmployeeSQL/sql_code.sql
@@ -57,6 +57,11 @@ join employees on dpt_manager.emp_no = employees.emp_no
 join departments on dpt_manager.dept_no = departments.dept_no;
 
 
-
-
+-- List the department of each employee with the following information:
+-- employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+join dpt_manager on employees.emp_no = dpt_manager.emp_no
+join dpt_emp on employees.emp_no = dpt_emp.emp_no
+join departments on departments.dept_no = dpt_manager.dept_no;
 
