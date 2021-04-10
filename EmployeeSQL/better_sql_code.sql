552cdebdc97f963201291f7594ae18858b858869
clem1803 <clementboit@gmail.com>
2021-04-10 14:49:23 -0400

    final work

--- EmployeeSQL/better_sql_code.sql
+++ EmployeeSQL/better_sql_code.sql
@@ -1,70 +1,70 @@
--- CREATE TABLE Departments (
---     dept_no varchar(255)   NOT NULL,
---     dept_name varchar(255)   NOT NULL,
---     CONSTRAINT pk_Departments PRIMARY KEY (
---         dept_no
---      )
--- );
+ CREATE TABLE Departments (
+     dept_no varchar(255)   NOT NULL,
+     dept_name varchar(255)   NOT NULL,
+     CONSTRAINT pk_Departments PRIMARY KEY (
+         dept_no
+      )
+ );
 
--- CREATE TABLE Dpt_emp (
---     emp_no int   NOT NULL,
---     dept_no varchar(255)   NOT NULL,
---     CONSTRAINT pk_Dpt_emp PRIMARY KEY (
---         emp_no,dept_no
---      )
--- );
+ CREATE TABLE Dpt_emp (
+     emp_no int   NOT NULL,
+     dept_no varchar(255)   NOT NULL,
+     CONSTRAINT pk_Dpt_emp PRIMARY KEY (
+         emp_no,dept_no
+      )
+ );
 
--- CREATE TABLE Dpt_manager (
---     dept_no varchar(255)   NOT NULL,
---     emp_no int   NOT NULL,
---     CONSTRAINT pk_Dpt_manager PRIMARY KEY (
---         dept_no,emp_no
---      )
--- );
+ CREATE TABLE Dpt_manager (
+     dept_no varchar(255)   NOT NULL,
+     emp_no int   NOT NULL,
+     CONSTRAINT pk_Dpt_manager PRIMARY KEY (
+         dept_no,emp_no
+      )
+ );
 
--- CREATE TABLE Employees (
---     emp_no int   NOT NULL,
---     emp_title_id varchar(255)   NOT NULL,
---     birth_date date   NOT NULL,
---     first_name varchar(255)   NOT NULL,
---     last_name varchar(255)   NOT NULL,
---     sex varchar(255)   NOT NULL,
---     hire_data date   NOT NULL,
---     CONSTRAINT pk_Employees PRIMARY KEY (
---         emp_no
---      )
--- );
+ CREATE TABLE Employees (
+     emp_no int   NOT NULL,
+     emp_title_id varchar(255)   NOT NULL,
+     birth_date date   NOT NULL,
+     first_name varchar(255)   NOT NULL,
+     last_name varchar(255)   NOT NULL,
+     sex varchar(255)   NOT NULL,
+     hire_data date   NOT NULL,
+     CONSTRAINT pk_Employees PRIMARY KEY (
+         emp_no
+      )
+ );
 
--- CREATE TABLE Salaries (
---     emp_no int   NOT NULL,
---     salary int   NOT NULL
--- );
+ CREATE TABLE Salaries (
+     emp_no int   NOT NULL,
+     salary int   NOT NULL
+ );
 
--- CREATE TABLE Titles (
---     title_id varchar(255)   NOT NULL,
---     title varchar(255)   NOT NULL,
---     CONSTRAINT pk_Titles PRIMARY KEY (
---         title_id
---      )
--- );
+ CREATE TABLE Titles (
+     title_id varchar(255)   NOT NULL,
+     title varchar(255)   NOT NULL,
+     CONSTRAINT pk_Titles PRIMARY KEY (
+         title_id
+      )
+ );
 
--- ALTER TABLE Dpt_emp ADD CONSTRAINT "fk_Dpt_emp_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES Employees ("emp_no");
+ ALTER TABLE Dpt_emp ADD CONSTRAINT "fk_Dpt_emp_emp_no" FOREIGN KEY("emp_no")
+ REFERENCES Employees ("emp_no");
 
--- ALTER TABLE Dpt_emp ADD CONSTRAINT "fk_Dpt_emp_dept_no" FOREIGN KEY("dept_no")
--- REFERENCES Departments ("dept_no");
+ ALTER TABLE Dpt_emp ADD CONSTRAINT "fk_Dpt_emp_dept_no" FOREIGN KEY("dept_no")
+ REFERENCES Departments ("dept_no");
 
--- ALTER TABLE Dpt_manager ADD CONSTRAINT "fk_Dpt_manager_dept_no" FOREIGN KEY("dept_no")
--- REFERENCES Departments ("dept_no");
+ ALTER TABLE Dpt_manager ADD CONSTRAINT "fk_Dpt_manager_dept_no" FOREIGN KEY("dept_no")
+ REFERENCES Departments ("dept_no");
 
--- ALTER TABLE Dpt_manager ADD CONSTRAINT "fk_Dpt_manager_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES Employees ("emp_no");
+ ALTER TABLE Dpt_manager ADD CONSTRAINT "fk_Dpt_manager_emp_no" FOREIGN KEY("emp_no")
+ REFERENCES Employees ("emp_no");
 
--- ALTER TABLE Employees ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
--- REFERENCES Titles ("title_id");
+ ALTER TABLE Employees ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+ REFERENCES Titles ("title_id");
 
--- ALTER TABLE Salaries ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES Employees ("emp_no");
+ ALTER TABLE Salaries ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+ REFERENCES Employees ("emp_no");
 
 
 -- Question 1: List the following details of each employee: employee number, last name, first name,
@@ -119,13 +119,42 @@ where first_name = 'Hercules' and last_name like 'B%' ;
 select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 from employees
 join dpt_emp on employees.emp_no = dpt_emp.emp_no
-join departments on dpt_emp.dept_no = departments.dept_no ;
---where departments.dept_name = 'Sales department' ;
+join departments on dpt_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales' ;
 
 
-Select * from employees where emp_no in (57444,13616,240129,427958) ;
+-- Question 7: List all employees in the Sales and Development departments, including 
+--their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+join dpt_emp on employees.emp_no = dpt_emp.emp_no
+join departments on dpt_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales' or departments.dept_name = 'Development';
 
+
+-- Question 8: In descending order, list the frequency count of employee last names,
+-- i.e., how many employees share each last name.
+select last_name, count (last_name)
+from employees
+group by last_name 
+order by last_name desc ;
+
+
+-- Bonus
+Select * from employees where emp_no in (499942,57444,13616,240129,427958) ;
 Select * from employees where emp_no = 57444 or emp_no = 13616 or emp_no = 240129 or emp_no = 427958;
+
+-- Create a histogram to visualize the most common salary ranges for employees.
+SELECT min(salary), max(salary)
+from salaries;
+
+
+-- Create a bar chart of average salary by title.
+select titles.title, round(avg(salaries.salary),2) as Salary_Average
+from titles
+join employees on titles.title_id = employees.emp_title_id
+join salaries on employees.emp_no = salaries.emp_no
+group by titles.title ;
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # sql-challenge
+Hi, welcome to my SQL query code.
+
+I tried to make those files as easy as possible to read.
+You can see the picture of my ERD and the .sql table schemata in the main repo.
+You can see my queries in the "EmployeeSQL" folder under the file "better_sql_code.sql"
+
+For the bonus part.
+I did all the work and it run.
+For security purpose I am not going to link my postgresql password.
+In the jupyter notebook on line 3, you will have to input your own password to run the code - i think.
+I am not quite sure how this works since i have never done this before. 
+
+
+Employee ID 499942 has the name of: April Foolsday
+
+Let me know if you have any problem.
